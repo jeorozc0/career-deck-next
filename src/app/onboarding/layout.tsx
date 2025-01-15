f4df3c95c4064420c1799a635f4ba6194a46cad1
Julio Orozco <89562415+jeorozc0@users.noreply.github.com>
2025-01-15 12:08:17 -0500

    create onboarding layout

--- src/app/onboarding/layout.tsx
+++ src/app/onboarding/layout.tsx
@@ -1,9 +1,15 @@
+import { FormNavigation } from "@/components/onboarding/Stepper";
 import { ReactNode } from "react";
 
 export default function OnboardingLayout({ children }: { children: ReactNode }) {
+
   return (
     <main className="flex flex-col h-screen w-screen">
-      <div className="h-full w-full p-40">{children}</div>
+      <div className="h-full w-full px-40 py-20">
+        <FormNavigation >
+          {children}
+        </FormNavigation>
+      </div>
     </main>
   )
 }
